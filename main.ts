0e1de329eb46b6eaff4039b0335072c55b3c9c15
Ryoda1 <ryoda41@gmail.com>
2024-10-20 09:57:53 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,1 @@
-const fruits: string[] = ["apple", "banana", "orange"];
-
-function greet(name: string, tree: number, niga?: string): string {
-    return `Hello, ${name + ' ' + tree + ' ' + niga}!`;
-}
-
-console.log(greet("world", 4))
-console.log(fruits)
+console.log("Hello World")
